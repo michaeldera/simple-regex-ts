962aef62ec98ba6d42a3f55500b68ca6d15b1805
Michael Dera <michaeldera@live.com>
2019-08-04 23:46:27 +0200

    modified regex for id to mach country records

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export const SimpleRegex = {
     Netone: RegExp(/^(((\+)*263)|0)71[0-9]{7}$/gm),
     Telecel: RegExp(/^(((\+)*263)|0)73[0-9]{7}$/gm),
   },
-  NationalID: RegExp(/^[0-9]{2}(-|s)[0-9]{5,9}s*[A-Z]s*[0-9]{2}$/gm),
+  NationalID: RegExp(/^[0-9]{2}(-|s)[0-9]{6,7}s*[A-Z]s*[0-9]{2}$/gm),
   NumberPlate: RegExp(/^[A-Z]{3}(-|[[:blank:]])[0-9]{3}$/gm),
   PassportNumber: RegExp(/^[A-Z]{2}[0-9]{6}$/gm),
 };
