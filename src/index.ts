1f36457cb33c3773175da6cf241b4e6de9853ec2
Michael Dera <michaeldera@live.com>
2019-10-23 21:10:10 +0200

    code reorg

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-/** Common Zimbabwean Regex patterns */
-export const SimpleRegex = {
-  EmailAddress: RegExp('^[a-zA-Z0-9.!#$%&’+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$'),
-  MobileNumber: {
+export const EmailAddress = RegExp('^[a-zA-Z0-9.!#$%&’+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$');
+export const  MobileNumber = {
     Econet: RegExp(/^(((\+)*263)|0)7(7|8)[0-9]{7}$/gm),
     Netone: RegExp(/^(((\+)*263)|0)71[0-9]{7}$/gm),
     Telecel: RegExp(/^(((\+)*263)|0)73[0-9]{7}$/gm),
-  },
-  NationalID: RegExp(/^[0-9]{2}(-|s)[0-9]{6,7}s*[A-Z]s*[0-9]{2}$/gm),
-  NumberPlate: RegExp(/^[A-Z]{3}(-|[[:blank:]])[0-9]{3}$/gm),
-  PassportNumber: RegExp(/^[A-Z]{2}[0-9]{6}$/gm),
 };
+  
+export const NationalID = RegExp(/^[0-9]{2}(-|s)[0-9]{6,7}s*[A-Z]s*[0-9]{2}$/gm);
+
+export const NumberPlate = RegExp(/^[A-Z]{3}(-|[[:blank:]])[0-9]{3}$/gm);
+export const  PassportNumber = RegExp(/^[A-Z]{2}[0-9]{6}$/gm);
+
+export const SimpleRegex = {
+  EmailAddress: EmailAddress,
+  MobileNumber: MobileNumber,
+  NationalID: NationalID,
+  NumberPlate: NumberPlate, 
+  PassportNumber: PassportNumber
+}
