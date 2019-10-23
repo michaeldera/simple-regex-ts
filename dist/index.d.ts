fde6999c34e9e748d90eb9790bb2a11804812dfa
Michael Dera <michaeldera@live.com>
2019-10-24 00:06:36 +0200

    added docs

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,12 +1,35 @@
-/** Common Zimbabwean Regex patterns */
+export declare const EmailAddress: RegExp;
+export declare const MobileNumber: {
+    Econet: RegExp;
+    Netone: RegExp;
+    Telecel: RegExp;
+};
+export declare const NationalID: RegExp;
+export declare const NumberPlate: RegExp;
+export declare const PassportNumber: RegExp;
 export declare const SimpleRegex: {
+    /**
+     * Provides regular expression for email addresses
+     */
     EmailAddress: RegExp;
+    /**
+     * Regular expressions for mobile phone network operators in Zimbabwe.
+     */
     MobileNumber: {
         Econet: RegExp;
         Netone: RegExp;
         Telecel: RegExp;
     };
+    /**
+    * Regular expression for National Identity Numbers in Zimbabwe
+    */
     NationalID: RegExp;
+    /**
+    * Regular expression for ordinary motor vehicle licence plate numbers in Zimbabwe.
+    */
     NumberPlate: RegExp;
+    /**
+  * Regular expression passport numbers in Zimbabwe.
+  */
     PassportNumber: RegExp;
 };
--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-/** Common Zimbabwean Regex patterns */
+exports.EmailAddress = RegExp('^[a-zA-Z0-9.!#$%&’+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$');
+exports.MobileNumber = {
+    Econet: RegExp(/^(((\+)*263)|0)7(7|8)[0-9]{7}$/gm),
+    Netone: RegExp(/^(((\+)*263)|0)71[0-9]{7}$/gm),
+    Telecel: RegExp(/^(((\+)*263)|0)73[0-9]{7}$/gm),
+};
+exports.NationalID = RegExp(/^[0-9]{2}(-|s)[0-9]{6,7}s*[A-Z]s*[0-9]{2}$/gm);
+exports.NumberPlate = RegExp(/^[A-Z]{3}(-|[[:blank:]])[0-9]{3}$/gm);
+exports.PassportNumber = RegExp(/^[A-Z]{2}[0-9]{6}$/gm);
 exports.SimpleRegex = {
-    EmailAddress: RegExp('^[a-zA-Z0-9.!#$%&’+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$'),
-    MobileNumber: {
-        Econet: RegExp(/^(((\+)*263)|0)7(7|8)[0-9]{7}$/gm),
-        Netone: RegExp(/^(((\+)*263)|0)71[0-9]{7}$/gm),
-        Telecel: RegExp(/^(((\+)*263)|0)73[0-9]{7}$/gm),
-    },
-    NationalID: RegExp(/^[0-9]{2}(-|s)[0-9]{5,9}s*[A-Z]s*[0-9]{2}$/gm),
-    NumberPlate: RegExp(/^[A-Z]{3}(-|[[:blank:]])[0-9]{3}$/gm),
-    PassportNumber: RegExp(/^[A-Z]{2}[0-9]{6}$/gm),
+    /**
+     * Provides regular expression for email addresses
+     */
+    EmailAddress: exports.EmailAddress,
+    /**
+     * Regular expressions for mobile phone network operators in Zimbabwe.
+     */
+    MobileNumber: exports.MobileNumber,
+    /**
+    * Regular expression for National Identity Numbers in Zimbabwe
+    */
+    NationalID: exports.NationalID,
+    /**
+    * Regular expression for ordinary motor vehicle licence plate numbers in Zimbabwe.
+    */
+    NumberPlate: exports.NumberPlate,
+    /**
+  * Regular expression passport numbers in Zimbabwe.
+  */
+    PassportNumber: exports.PassportNumber
 };
